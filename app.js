df99107835fa35ccf819749c6d55c34a744b927e
Evan Turner <evturn@gmail.com>
2014-12-29 17:02:55 -0500

    View1 created inside app.js

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 var View1 = Backbone.View.extend({
-	
+	initialize: function() {
+		this.render();
+	},
+	render: function() {
+		this.$el.html(this.model.get('Message') + " from the View 1");
+	}
 });

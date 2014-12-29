694f9cb0a8006d25295bb872deced449037ac051
Evan Turner <evturn@gmail.com>
2014-12-29 17:19:38 -0500

    View3 added to the app

--- app.js
+++ app.js
@@ -15,3 +15,12 @@ var View2 = Backbone.View.extend({
 		this.$el.html(this.model.get('Message') + " from the View 2");
 	}
 });
+
+var View3 = Backbone.View.extend({
+	initialize: function() {
+		this.render();
+	},
+	render: function() {
+		this.$el.html(this.model.get('Message') + " from the View 3");
+	}
+});
